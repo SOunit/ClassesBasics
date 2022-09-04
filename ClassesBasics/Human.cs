2e8527f49355902780636f9574e2d20751657781
SOunit <workbookwork7@gmail.com>
2022-09-04 12:26:37 -0700

    Add project files.

---
+++ ClassesBasics/Human.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassesBasics
+{
+    internal class Human
+    {
+        // member variable
+        private string firstName;
+        private string lastName;
+
+        // ctor
+        public Human(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        // member
+        public void IntroduceMyself()
+        {
+            Console.WriteLine("Hi I'm {0} {1}", firstName, lastName);
+        }
+    }
+}
---
+++ ClassesBasics/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ClassesBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // instance of Human class
+            Human jack = new Human("Jack", "Pearson");
+
+            // call method of a class
+            jack.IntroduceMyself();
+
+            Human bec = new Human("Bec", "Pearson");
+            bec.IntroduceMyself();
+        }
+    }
+}
